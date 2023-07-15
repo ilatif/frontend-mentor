8bef8d0e6729918a6412babaeb9cc8e19372e068
Imran Latif <ilatif.bwp@gmail.com>
2023-07-15 18:45:41 +0500

    Todo App: Disable checkbox button with add todo input

--- todo-app/src/AddTodo.tsx
+++ todo-app/src/AddTodo.tsx
@@ -3,7 +3,7 @@ import './AddTodo.css';
 function AddTodo(props: { addTodo: (text: string) => void }) {
   return <>
     <div className="add-todo-container">
-      <input type="checkbox" className="todo-circle" disabled={false} />
+      <input type="checkbox" className="todo-circle" disabled={true} />
       <input type="text" placeholder="Create a new todo..." className="todo-input" onKeyUp={(e) => {
         if (e.keyCode === 13) {
           const target = e.target as HTMLInputElement;
